1cf448208c2ed2f913f7782d7b60cd323b0c7ed7
sherif sadek <sherif_h_sadek@yahoo.com>
2023-04-20 14:03:37 -0300

    trigger pipe

--- my-tracker-frontend/src/App.js
+++ my-tracker-frontend/src/App.js
@@ -2,17 +2,9 @@ import './App.css';
 import Main from './main/Main';
 import 'bootstrap/dist/css/bootstrap.css';
 
-
 function App() {
   return (
-    // <Container>
-    //   <Header className="w-75 general-border" version="Version 1.0"/>
-    //   <Main className="w-75 general-border" />
-    //   {/* <Footer className="w-50 general-border" /> */}
-    // </Container>
-    <>
     <Main/>
-    </>
   );
 }
 
